9a206cd02a85c0bfd179071855d434075c067289
DonCerebro1 <tba^C>
2021-11-23 11:33:16 +0100

    first Push

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,50 @@
 package com.company;
 
+import com.company.ui.MainUI;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                CreateGUI();
+
+            }
+        });
     }
+
+private static void CreateGUI()
+{
+    MainUI ui = new MainUI();
+    JPanel root = ui.getRootPanel();
+    JFrame mainFrame = new JFrame();
+    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    mainFrame.setContentPane(root);
+    mainFrame.pack();
+    mainFrame.setTitle("E-Meter");
+    mainFrame.setLocationRelativeTo(null);
+    mainFrame.setVisible(true);
+
+    JMenuBar bar = new JMenuBar();
+    JMenu menu = new JMenu("Settings");
+    bar.add(menu);
+    bar.setVisible(true);
+    menu.setVisible(true);
+
+
+
+}
+
+private static void MenuBar()
+{
+    JMenuBar bar = new JMenuBar();
+    JMenu menu = new JMenu("Settings");
+    bar.add(menu);
+    bar.setVisible(true);
+    menu.setVisible(true);
+}
 }
---
+++ src/com/company/ui/MainUI.java
@@ -0,0 +1,17 @@
+package com.company.ui;
+
+import javax.swing.*;
+
+public class MainUI {
+    private JPanel rootPanel;
+
+
+
+
+public JPanel getRootPanel()
+{
+    return rootPanel;
+}
+
+}
+
